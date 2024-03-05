cf16f35aed68826ae7ef564799117c9620502225
mrWinsont <aleksandrgrin263@gmail.com>
2024-03-05 19:48:11 +0200

    +Task 2

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -3,3 +3,15 @@ $very_bad_unclear_name = "14 chicken wings";
 $order =& $very_bad_unclear_name;
 $order.= " in my plate";
 echo "\nYour order is: $very_bad_unclear_name.";
+
+$varInt = 77777;
+echo "\n$varInt";
+echo "\n";
+$varFloat = 77.777;
+echo "$varFloat";
+
+echo "\n",5+7;
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo "\n", $lastMonth - $thisMonth;
