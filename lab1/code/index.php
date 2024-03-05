95dfda74cb45fdb2c399eb98acc746e77c743f82
mrWinsont <aleksandrgrin263@gmail.com>
2024-03-05 21:35:15 +0200

    +Task 14

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,7 +1,7 @@
 <?php
 $very_bad_unclear_name = "14 chicken wings";
 $order =& $very_bad_unclear_name;
-$order.= " in my plate";
+$order.= " with double burger, fries, cola, cheese sauce, and a kiss on the cheek";
 echo "\nYour order is: $very_bad_unclear_name.";
 
 $varInt = 77777;
@@ -26,9 +26,71 @@ echo "\n",8**2;
 
 $myNum = 7;
 $answer = $myNum;
-$answer+=2;
-$answer*=2;
-$answer-=2;
-$answer/=2;
-$answer-=$myNum;
+$answer += 2;
+$answer *= 2;
+$answer -= 2;
+$answer /= 2;
+$answer -= $myNum;
 echo "\n$answer";
+
+$a = 10;
+$b = 3;
+$ost = $a % $b;
+if($a % $b == 0){
+    echo "\nделится ",$a / $b;
+}
+else{
+    echo "\n делится с остатком ",$ost;
+}
+$st = pow(2,10);
+$sq = sqrt(245);
+$arr = [4, 2, 5, 19, 13, 0, 10];
+$sum = 0;
+foreach ($arr as $i){
+    $sum += pow($i,2);
+}
+$sumSqrt = sqrt($sum);
+$tmpNum = 379;
+round(sqrt($tmpNum),0); //до целых
+round(sqrt($tmpNum),1); //до десятых
+round(sqrt($tmpNum),2); //до сотых
+
+$arr1 = ["floor" => floor(sqrt(587)), "ceil" => ceil(sqrt(587))];
+$arr2 = [ 4, -2, 5, 19, -130, 0, 10];
+$minNumArr2 = min($arr2);
+$maxNumArr2 = max($arr2);
+
+echo "\n",rand(1,100);
+$arr3 = [];
+for($i = 0;$i<10;$i++){
+    $arr[$i] = rand(1,1000);
+}
+
+$a = 5;
+$b = 10;
+echo "\n", abs($a - $b);
+
+$arr4 = [1, 2, -1, -2, 3, -3];
+for ($i = 0;$i < count($arr4);$i++){
+    $arr4[$i] = abs($arr4[$i]);
+}
+
+$num = 50;
+$ansArr = [];
+for ($i = 1; $i<=$num;$i++){
+    if($num % $i == 0){
+        array_push($ansArr,$i);
+    }
+}
+
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$count = 0;
+$sumOfFirst = 0;
+foreach($arr as $i){
+    $count++;
+    $sumOfFirst += $i;
+    if($sumOfFirst > 10){
+        break;
+    }
+}
+echo "\n",$count;
