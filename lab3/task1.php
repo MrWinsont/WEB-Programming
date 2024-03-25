82f749ee4634b51fb96da6a7a01eb861868dbfbc
mrWinsont <aleksandrgrin263@gmail.com>
2024-03-25 17:45:37 +0200

    + task1.B

--- lab3/task1.php
+++ lab3/task1.php
@@ -6,3 +6,15 @@ $matches = [];
 $count = preg_match_all($regexp, $str1, $matches);
 echo "\nстрок найдено $count\n";
 var_dump($matches);
+
+$str2 =  'a1b2c3';
+echo "\n$str2";
+$regexp1 = '/[0-9]/';
+$matches = [];
+preg_match_all($regexp1, $str2, $matches);
+$tmpArray = $matches[0];
+for($i = 0;$i<count($matches[0]);$i++){
+    $regexp2  = "/[$tmpArray[$i]]/";
+    $str2 = preg_replace($regexp2, pow($matches[0][$i],3), $str2);
+}
+echo "\n$str2";
