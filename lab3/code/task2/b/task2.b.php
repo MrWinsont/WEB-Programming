f174096394f4685dfd12579c20939f51312dd808
mrWinsont <aleksandrgrin263@gmail.com>
2024-03-26 18:13:30 +0200

    +task2.b

---
+++ lab3/code/task2/b/task2.b.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset = "UTF = 8">
+    <title>task2</title>
+</head>
+
+<body>
+    <form method = "post" action="session.php">
+        <label>What is your Surname?</label>
+        <input type = "text" name = "surname" required><br>
+
+        <label>What is your Name?</label>
+        <input type = "text" name = "name" required><br>
+
+        <label>How old are you?</label>
+        <input type = "text" name = "age" required><br>
+
+        <input type = "submit" value = "accept">
+    </form>
+
+</body>
+</html>
